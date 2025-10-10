8b8660f41c0f248c61665370a2bc89607469e9c5
William Baker <wgdb198@yahoo.com>
2025-10-10 23:23:14 +0100

    edited elevator carriage process method and created new issues

--- game-jam-seventy-three-pulley-tower/elevator_carriage.gd
+++ game-jam-seventy-three-pulley-tower/elevator_carriage.gd
@@ -5,7 +5,7 @@ var currentFloor = 0
 # current floor at. if not at a floor = -100
 var currentFloorPosition = 0
 var maxFloor = 1
-var velocity = 0
+var new_velocity = 0
 var initial_child_count = 0
 var max_capacity = 4
 @export var material_scene: PackedScene
@@ -18,50 +18,23 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	currentFloor = get_parent().currentFloor
-	velocity = get_parent().velocity.y
-	if velocity > 0:
-		position.y -= delta * 200
-		if currentFloor == 2:
-			if position.y < 255:
-				position.y = 255
-				velocity = 0
-				currentFloorPosition = 2
-				get_parent().get_parent().get_node("WorkerCollection").set_all_workers_not_stopped(2)
-			else:
-				currentFloorPosition = -100
-		if currentFloor == 1:
-			if position.y < 395:
-				position.y = 395
-				velocity = 0
-				currentFloorPosition = 1
-				get_parent().get_parent().get_node("WorkerCollection").set_all_workers_not_stopped(1)
-			else:
-				currentFloorPosition = -100
-				
-	if velocity < 0:
-		position.y += delta * 200
-		if currentFloor == 1:
-			if position.y > 395:
-				position.y  = 395
-				velocity = 0
-				currentFloorPosition = 1
-				get_parent().get_parent().get_node("WorkerCollection").set_all_workers_not_stopped(1)
-			else:
-				currentFloorPosition = -100
-		if currentFloor == 0:
-			
-						
-			if position.y > 533:
-				position.y = 533
-				velocity = 0
-				currentFloorPosition = 0
-				get_parent().get_parent().get_node("WorkerCollection").set_all_workers_not_stopped(0)
-			else:
-				currentFloorPosition = -100
+	currentFloor = get_parent().get_current_floor()
+	new_velocity = get_parent().get_velocity_y()
+	position.y += delta * 200 * new_velocity
+	
+	var test = position.y + (140 * currentFloor)
+	
+		
+	if (test  < 533 + 10)&& (test > 533) :
+		get_parent().get_parent().get_node("WorkerCollection").set_all_workers_not_stopped(currentFloor)
+		new_velocity = 0
+		get_parent().set_velocity_y(0)
+		position.y = 533 -(140 * currentFloor)
+		currentFloorPosition = currentFloor
+	
 
 	if (get_child_count()>initial_child_count):
-		var material_placeholder  = get_child(initial_child_count) 
+		var material_placeholder  = get_child(initial_child_count-1) 
 		if (material_placeholder.get_node("MaterialNameComponent").get_material_name() == Materials.STEEL):
 			material_placeholder.position = Vector2(-4.5,-4.0)
 			material_placeholder.scale = Vector2(0.45,0.45)
@@ -70,7 +43,7 @@ func _process(delta: float) -> void:
 			material_placeholder.scale = Vector2(0.3,0.3)
 	
 	if (get_child_count()>initial_child_count+1):
-		var material_placeholder  = get_child(initial_child_count+1) 
+		var material_placeholder  = get_child(initial_child_count) 
 		if (material_placeholder.get_node("MaterialNameComponent").get_material_name() == Materials.STEEL):
 			material_placeholder.position = Vector2(5.5,-4.0)
 			material_placeholder.scale = Vector2(0.45,0.45)
@@ -79,7 +52,7 @@ func _process(delta: float) -> void:
 			material_placeholder.scale = Vector2(0.3,0.3)
 			
 	if (get_child_count()>initial_child_count+2):
-		var material_placeholder  = get_child(initial_child_count+2) 
+		var material_placeholder  = get_child(initial_child_count+1) 
 		if (material_placeholder.get_node("MaterialNameComponent").get_material_name() == Materials.STEEL):
 			material_placeholder.position = Vector2(-4.5,7.0)
 			material_placeholder.scale = Vector2(0.45,0.45)
@@ -89,7 +62,7 @@ func _process(delta: float) -> void:
 			
 	if (get_child_count()>initial_child_count+3):
 
-		var material_placeholder  = get_child(initial_child_count+3) 
+		var material_placeholder  = get_child(initial_child_count+2) 
 		if (material_placeholder.get_node("MaterialNameComponent").get_material_name() == Materials.STEEL):
 			material_placeholder.position = Vector2(5.5,7.0)
 			material_placeholder.scale = Vector2(0.45,0.45)
@@ -97,23 +70,12 @@ func _process(delta: float) -> void:
 			material_placeholder.position = Vector2(5.0,7.0)
 			material_placeholder.scale = Vector2(0.3,0.3)
 	
-	get_parent().velocity.y = velocity
+	get_parent().set_velocity_y(new_velocity)
 	
 func setMaxFloor(newMaxFloor) -> void:
 	maxFloor = newMaxFloor
 
 
-func _on_textured_up_button_pressed() -> void:
-	velocity = 1
-	if currentFloor < maxFloor:
-		currentFloor += 1
-
-
-func _on_texture_down_button_pressed() -> void:
-	velocity = -1
-	if currentFloor > 0:
-		currentFloor -= 1
-		
 func getCurrentFloor() -> int:
 	return currentFloor
 	
@@ -121,7 +83,7 @@ func get_current_floor_position() -> int:
 	return currentFloorPosition 
 	
 func getVelocity() -> int:
-	return velocity
+	return new_velocity
 	
 func has_materials() -> bool:
 	return get_child_count() > 0
